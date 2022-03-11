383eed10167eaf2fdade0c39341be631f48a1ca6
HarshadChovatiya <harshadchovatiya9@gmail.com>
2022-03-11 10:05:11 +0530

    added testcase to handle multiple numbers with default delimiter

--- src/stringcalculator/TestStringCalculator.java
+++ src/stringcalculator/TestStringCalculator.java
@@ -31,4 +31,10 @@ public class TestStringCalculator {
         int output = stringCalculator.Add("5,4");
         assertEquals(9, output);
     }
+
+    @Test
+    void testMultipleNumbersWithDefaultDelimiter() {
+        int output = stringCalculator.Add("5,4,3,2,1");
+        assertEquals(15, output);
+    }
 }
